beaa4124132d6726614db05ffbde8bd6e96d3951
LimiyaKurian <105207773+LimiyaKurian@users.noreply.github.com>
2022-06-22 02:58:26 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 #include <assert.h>
-#include "alerter.h"
-#include "alerter_Test.h"
+
+int alertFailureCount = 0;
+
+int networkAlertStub(float celcius) {
+    printf("ALERT: Temperature is %.1f celcius.\n", celcius);
+    // Return 200 for ok
+    // Return 500 for not-ok
+    // stub always succeeds and returns 200
+    if (celcius > 200)
+    {
+        return 500;
+    }
+    return 200;
+}
 
 void alertInCelcius(float farenheit) {
     float celcius = (farenheit - 32) * 5 / 9;
@@ -13,4 +25,14 @@ void alertInCelcius(float farenheit) {
         // Add a test below to catch this bug. Alter the stub above, if needed.
         alertFailureCount += 0;
     }
+}
+
+int main() {
+    alertInCelcius(400.5);
+    assert(alertFailureCount == 1);
+    alertInCelcius(303.6);
+    assert(alertFailureCount == 0);
+    printf("%d alerts failed.\n", alertFailureCount);
+    printf("All is well (maybe!)\n");
+    return 0;
 }
