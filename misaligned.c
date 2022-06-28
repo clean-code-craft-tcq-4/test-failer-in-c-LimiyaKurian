1df88eebf6a96db6a636765cce4f54bedb965c87
LimiyaKurian <105207773+LimiyaKurian@users.noreply.github.com>
2022-06-29 01:24:55 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -13,7 +13,7 @@ int printColorMap() {
     for(majorColorCount = 0; majorColorCount < numberOfMajorColors; majorColorCount++) {
         for(minorColorCount = 0; minorColorCount < numberOfMinorColors; minorColorCount++) {
         int pairnumber = ColorpairNumber(majorColorCount,minorColorCount);
-        printf("%d \t %s  \t %s\n", pairnumber, majorColor[majorColorCount], minorColor[minorColorCount]);
+        printf("%d\t  %s\t  %s\n", pairnumber, majorColor[majorColorCount], minorColor[minorColorCount]);
         }
     }
     
