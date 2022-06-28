58da0db1b62b6148c984ba55968ea9f0d38c0734
LimiyaKurian <105207773+LimiyaKurian@users.noreply.github.com>
2022-06-29 02:30:34 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -2,27 +2,41 @@
 #include <assert.h>
 #include "misaligned.h"
 
-int ColorpairNumber(int majorIndex , int minorIndex)
-{
-	return ((majorIndex * numberOfMajorColors) + minorIndex+1);
+int GetPairNumberFromColor(const ColorPair_t* colorPair_t) {
+    return colorPair->majorColor * numberOfMinorColors +
+            colorPair->minorColor + 1;
 }
 
 int printColorMap() {
 
     int majorColorCount =0 ,minorColorCount = 0;
+	ColorPair colorpair;
     for(majorColorCount = 0; majorColorCount < numberOfMajorColors; majorColorCount++) {
         for(minorColorCount = 0; minorColorCount < numberOfMinorColors; minorColorCount++) {
-        int pairnumber = ColorpairNumber(majorColorCount,minorColorCount);
-        printf("%d\t|\t%s\t|\t%s\n", pairnumber, majorColor[majorColorCount], minorColor[minorColorCount]);
+		colorpair.majorColor = (MajorColor)majorColorCount;
+                colorpair.minorColor = (MinorColor)minorColorCount;
+		int pairnumber = GetPairNumberFromColor(&colorPair);
+                printf("%d\t|\t%s\t|\t%s\n", pairnumber, majorColor[majorColorCount], minorColor[minorColorCount]);
         }
     }
     
     return majorColorCount * minorColorCount;
 }
 
+void testPairToNumber(MajorColor major, MinorColor minor, int expectedPairNumber)
+{
+    ColorPair colorPair;
+    colorPair.majorColor = major;
+    colorPair.minorColor = minor;
+    int pairNumber = GetPairNumberFromColor(&colorPair);
+    assert(pairNumber == expectedPairNumber);
+}
+
 int main() {
     int result = printColorMap();
     assert(result == 25);
+    testPairToNumber(WHITE, BLUE,1);
+    testPairToNumber(YELLOW,SLATE,20);
     printf("All is well (maybe!)\n");
     return 0;
 }
