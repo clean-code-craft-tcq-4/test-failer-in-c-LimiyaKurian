260699c360c1bf4fb294bfe9b357dfae8fe91d5e
LimiyaKurian <105207773+LimiyaKurian@users.noreply.github.com>
2022-06-29 02:25:45 +0530

    Update misaligned.h

--- misaligned.h
+++ misaligned.h
@@ -4,3 +4,26 @@
  int numberOfMajorColors = sizeof(majorColor) / sizeof(majorColor[0]);
 	
  int numberOfMinorColors = sizeof(minorColor) / sizeof(minorColor[0]);
+
+typedef enum
+{
+	WHITE,
+	RED,
+	BLACK,
+	YELLOW,
+	VIOLET
+}MajorColor;
+
+typedef enum
+{
+	BLUE,
+	ORANGE,
+	GREEN,
+	BROWN,
+	SLATE
+}MinorColor;
+
+typedef struct {
+    MajorColor majorColor;
+    MinorColor minorColor;
+}ColorPair;
