57fe4879b1ca3b9d8625e62108dd96b855652594
LimiyaKurian <105207773+LimiyaKurian@users.noreply.github.com>
2022-06-29 01:23:10 +0530

    Update misaligned.h

--- misaligned.h
+++ misaligned.h
@@ -1,6 +1,6 @@
  const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
  const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 	
- int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+ int numberOfMajorColors = sizeof(majorColor) / sizeof(majorColor[0]);
 	
- int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+ int numberOfMinorColors = sizeof(minorColor) / sizeof(minorColor[0]);
